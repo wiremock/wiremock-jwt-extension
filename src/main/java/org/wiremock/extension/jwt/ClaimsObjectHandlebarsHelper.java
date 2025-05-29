20d1c561e40c28ef82ee98fe2502323b213828ba
Michalis Diamantopoulos <mikediamanto@gmail.com>
2025-05-29 09:20:26 +0300

    Added object extension helper

---
+++ src/main/java/org/wiremock/extension/jwt/ClaimsObjectHandlebarsHelper.java
@@ -0,0 +1,25 @@
+package org.wiremock.extension.jwt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.jknack.handlebars.Options;
+import com.github.tomakehurst.wiremock.extension.responsetemplating.helpers.HandlebarsHelper;
+
+public class ClaimsObjectHandlebarsHelper extends HandlebarsHelper<Object> {
+
+    @Override
+    public Object apply(Object context, Options options) {
+        Map<String, Object> result = new HashMap<>();
+
+        // Process each key-value pair from the options hash
+        options.hash.forEach((key, value) -> {
+            // Convert each key-value pair to a nested structure if the key contains dots
+            Map<String, Object> map = new HashMap<>();
+            map.put(key, value);
+            result.putAll(map);
+        });
+
+        return result;
+    }
+}
